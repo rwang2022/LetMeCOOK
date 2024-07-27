6374bf5de8bd463002028455ec8e3b6a2ee97e2c
Ryan Wang <ryanwang601@gmail.com>
2024-07-27 15:04:19 -0400

    erky got Google Auth to work until callback

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const passport = require('passport')
 const app = express()
 const port = 3000
 require('./auth')
 
-app.get('/', (req,res) => {
-    res.send('Hello World!')
-})
+app.get('/auth/google',
+    passport.authenticate('google', { scope:
+     [ 'email', 'profile' ] }
+));
 
-app.get('/helloWorld', (req,res) => {
-    res.send('Hello World! from hello')
+app.get('/auth/google/callback',
+    passport.authenticate('google', { 
+     successRedirect: '/auth/google/success',
+     failureRedirect: '/auth/google/failure'
+}));
+
+app.get('/', (req,res) => {
+    res.sendFile('index.html', {root: '.'})
 })
 
 app.listen(port, () => {
