6df1616fa050df6b93e8757a7c4e0b9ffd0678f7
Ryan Wang <ryanwang601@gmail.com>
2024-07-27 15:57:29 -0400

    got google setup working after signin

--- app.js
+++ app.js
@@ -1,24 +1,60 @@
 const express = require('express')
 const passport = require('passport')
+const session = require('express-session')
 const app = express()
 const port = 3000
+
 require('./auth')
 
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({
+    secret: 'mysecret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+}))
+
+
+app.use(passport.initialize());
+app.use(passport.session())
+
 app.get('/auth/google',
-    passport.authenticate('google', { scope:
-     [ 'email', 'profile' ] }
-));
+    passport.authenticate('google', {
+        scope:
+            ['email', 'profile']
+    }
+    ));
 
 app.get('/auth/google/callback',
-    passport.authenticate('google', { 
-     successRedirect: '/auth/google/success',
-     failureRedirect: '/auth/google/failure'
-}));
+    passport.authenticate('google', {
+        successRedirect: '/auth/protected',
+        failureRedirect: '/auth/google/failure'
+    }));
 
-app.get('/', (req,res) => {
-    res.sendFile('index.html', {root: '.'})
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: '.' })
+});
+
+
+app.get('/auth/google/failure', (req, res) => {
+    res.send("Something went wrong!")
+});
+
+app.get('/auth/protected', isLoggedIn, (req, res) => {
+    let name = req.user.displayName;
+    res.send(`Hello ${name}`);
+});
+
+app.use('/auth/logout', (req, res) => {
+    req.session.destroy();
+    res.send('See you again!');
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
--- auth.js
+++ auth.js
@@ -1,26 +1,23 @@
 
 require('dotenv').config();
-
-var passport = require('passport');
+const passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        done(null, profile);
-    });
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/callback",
+  passReqToCallback: true,
+},
+  function (request, accessToken, refreshToken, profile, done) {
+    done(null, profile);
   }
 ));
 
-passport.serializeUser((user, done)=>{
-    done(null, user)
+passport.serializeUser((user, done) => {
+  done(null, user)
 });
 
-passport.deserializeUser((user, done)=>{
-    done(null, user)
+passport.deserializeUser((user, done) => {
+  done(null, user)
 });
