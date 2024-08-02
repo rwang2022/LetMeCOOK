350f635e286350feed6f564848e8260affd75139
Ryan Wang <ryanwang601@gmail.com>
2024-08-02 19:29:45 -0400

    attempted to add ingredient to ingredients table

--- db.js
+++ db.js
@@ -12,3 +12,19 @@ client.connect().then((rsp) => {
 }).catch((err) => {
     console.log(err)
 })
+
+// ! UNTESTED CODE
+const app = express()
+
+app.post('/create_ingredient', async (req, res) => {
+    const text = 'INSERT INTO Ingredients(name, category) VALUES($1, $2) RETURNING *';
+    const values = ['eggs', 'poultry'];
+
+    try {
+        const result = await client.query(text, values);
+        res.status(201).send(result.rows[0]);
+    } catch (err) {
+        console.error(err.stack);
+        res.status(500).send('Error inserting ingredient');
+    }
+});
